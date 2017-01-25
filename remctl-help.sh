493eb6624a9ca76f82d40bfc358d394936db2f06
Alex Dehnert <adehnert@mit.edu>
2017-01-24 23:46:24 -0500

    Mention the "discuss" command explicitly

--- remctl-help.sh
+++ remctl-help.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 discuss server with a remctl-based interface for managing meetings.
 
-The following commands are available:
+The following subcommands of the "discuss" command are available:
 - make-public-meeting shortname "Long name": create a new public meeting (readable by anybody)
 - make-private-meeting shortname "Long name": create a new private meeting (initially readable by the creator only; see set-read-acl)
 - set-read-acl meeting moira-list: set the read ACL (rs) of <meeting> to <moira-list>; moira-list must be an AFS group
