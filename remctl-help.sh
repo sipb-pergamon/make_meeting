2729da94015e0cf2d827af2be93b45b3be3a21dd
Alex Dehnert <adehnert@mit.edu>
2017-01-24 23:33:55 -0500

    Add remctl config file and help message

---
+++ remctl-help.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cat <<EOF
+discuss server with a remctl-based interface for managing meetings.
+
+The following commands are available:
+- make-public-meeting shortname "Long name": create a new public meeting (readable by anybody)
+- make-private-meeting shortname "Long name": create a new private meeting (initially readable by the creator only; see set-read-acl)
+- set-read-acl meeting moira-list: set the read ACL (rs) of <meeting> to <moira-list>; moira-list must be an AFS group
+EOF
